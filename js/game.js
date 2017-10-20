188d54c28fbba83ffeacde20bcfed8a7b04f19ad
Your Name <83353583@qq.com>
2017-10-20 08:56:10 +0800

    添加花里胡哨

--- js/game.js
+++ js/game.js
@@ -6,23 +6,42 @@ window.onload = function () {
     let num = 10;
     let size = 81;
     let uncover = 0;
+    let numArr = [];
     //随机布雷
-    for (let i = 0; i < num; i++) {
-        innerNum[Math.floor(size * Math.random())].innerHTML = "*";
+    setMine();
+    setNum();
+    function setMine() {
+        for (let i = 0; i < num; i++) {
+            let pos = Math.floor(size * Math.random());
+            while (numArr.includes(pos)) {
+                pos = Math.floor(size * Math.random());
+            }
+            numArr[numArr.length] = pos;
+            innerNum[pos].innerHTML = "*";
+        }
+        console.log("mineOK");
     }
-    console.log(row[0].children[0].lastElementChild);
+
+    // console.dir(grow);
+    // console.dir(row);
+    // console.dir(grow[0].children[0].lastElementChild);
+    // console.dir(row[0].children[0].lastElementChild);
     //判断小方块周围雷数
-    for (let r = 0; r < row.length; r++) {
-        let col = row[r].querySelectorAll(".inner span");
-        for (let c = 0; c < col.length; c++) {
-            if (col[c]) {
-                if (!(col[c].innerHTML)) {
-                    col[c].innerHTML = getMine(r, c, getPos(r,c));
+    function setNum() {
+        for (let r = 0; r < row.length; r++) {
+            let col = row[r].querySelectorAll(".inner span");
+            for (let c = 0; c < col.length; c++) {
+                if (col[c]) {
+                    if (!(col[c].innerHTML)) {
+                        col[c].innerHTML = getMine(r, c, getPos(r, c));
+                    }
                 }
             }
-        }
 
+        }
+        console.log("numOK");
     }
+
     // 获得雷数Fn
     function getMine(r, c, pos) {
         let num = 0;
@@ -164,123 +183,203 @@ window.onload = function () {
                 num++;
             }
         }
-        if(num){
+        if (num) {
             return num;
-        }else{
+        } else {
             return "";
         }
 
     }
 
     // 判断区
-    function getPos(r,c){
+    function getPos(r, c) {
         let pos;
         if (r != 0 && r != row.length - 1 && c != 0 && c != row[0].children.length - 1) {
-            pos="m";
+            pos = "m";
         } else if (r === 0 && c != 0 && c != row[0].children.length - 1) {
-            pos="t";
+            pos = "t";
         } else if (r === row.length - 1 && c != 0 && c != row[0].children.length - 1) {
-            pos="b";
+            pos = "b";
         } else if (c === 0 && r != 0 && r != row.length - 1) {
-            pos="l";
+            pos = "l";
         } else if (c === row[0].children.length - 1 && r != 0 && r != row.length - 1) {
-            pos="r";
+            pos = "r";
         } else if (r === 0 && c === 0) {
-            pos="lt";
+            pos = "lt";
         } else if (r === 0 && c === row[0].children.length - 1) {
-            pos="rt";
+            pos = "rt";
         } else if (c === 0 && r === row.length - 1) {
-            pos="lb";
+            pos = "lb";
         } else if (c === row[0].children.length - 1 && r === row.length - 1) {
-            pos="rb";
+            pos = "rb";
+        }
+        if (r < 0 || c < 0) {
+            return;
         }
         return pos;
     }
+
     // 点击空白
-    function blankClick(r,c,pos){
+    function blankClear(r, c, pos) {
         if (pos === "m") {
             row[r - 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "t") {
             row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "b") {
             row[r - 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "l") {
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "r") {
             row[r - 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "lt") {
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "rt") {
             row[r + 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r + 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "lb") {
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c + 1].firstElementChild.classList.add("uncover");
+
             row[r].children[c + 1].firstElementChild.classList.add("uncover");
+
         }
         if (pos === "rb") {
-            row[r].children[c - 1].firstElementChild.classList.add("uncover");
             row[r - 1].children[c - 1].firstElementChild.classList.add("uncover");
+
             row[r - 1].children[c].firstElementChild.classList.add("uncover");
+
+            row[r].children[c - 1].firstElementChild.classList.add("uncover");
+
         }
     }
+
     //小方块添加点击事件
     coverObj.forEach(function (ele, index) {
+        // 第一下踩到雷
+        // 一般情况
         ele.onclick = function () {
             ele.classList.add("uncover");
-
-
             //踩到雷
             if (innerNum[index].innerHTML === "*") {
+                innerNum[index].innerHTML = "☠";
                 for (let i = 0; i < coverObj.length; i++) {
                     coverObj[i].classList.add("uncover");
                 }
             }
-            if(!innerNum[index].innerHTML){
-                let c=index%9;
-                let r=Math.floor(index/9);
-                blankClick(r, c, getPos(r,c));
+            // 点开空白块
+            if (!innerNum[index].innerHTML) {
+                let c = index % 9;
+                let r = Math.floor(index / 9);
+                blankClear(r, c, getPos(r, c));
             }
             //排完雷
-            uncover=document.querySelectorAll(".uncover").length;
+            uncover = document.querySelectorAll(".uncover").length;
             if (uncover === size - num) {
-                alert("win");
+                addStarFn(100);
+                coverObj.forEach(function (ele, index) {
+                    ele.innerHTML = "☻";
+                    ele.onclick = null;
+                })
             }
         }
     });
-
 };
+
+// 星星
+let starColor = ["0", "3", "6", "9", "c", "f"];
+function getColor() {
+    let str = "#";
+    for (let i = 0; i < 3; i++) {
+        let pos = Math.floor(6 * (Math.random() - 0.0001));
+        str += starColor[pos];
+    }
+    return str;
+}
+let icon = ["★", "❤", "❉", "❁", "☂", "☃", "☻", "☀", "☎"];
+function getIcon() {
+    let pos = Math.floor(9 * (Math.random() - 0.0001));
+    return icon[pos];
+}
+function addStarFn(starNum) {
+    for (let i = 0; i < starNum; i++) {
+        let star = document.createElement("div");
+        star.innerHTML = "★";
+        star.className = "star";
+        star.style.left = `${ Math.floor(Math.random() * innerWidth)}px`;
+        star.style.animationDuration = `${Math.floor(5 + 25 * Math.random())}s`;
+        star.style.fontSize = `${Math.floor(12 + 24 * Math.random())}px`;
+        star.style.color = getColor();
+        document.documentElement.appendChild(star);
+    }
+}
+
+
 
